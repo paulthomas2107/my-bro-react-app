3bc4f3e823a2f8b781d5940e181967b91778c1fd
Paul Thomas <paulthomas2107@gmail.com>
2023-12-11 09:04:19 +0000

    xxx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import Food from './Food';
+import MyComponent from './MyComponent';
 
 function App() {
   return (
     <>
       <Header />
       <Food />
-      <Food />s
+      <Food />
+      <MyComponent />
       <Footer />
     </>
   );
---
+++ src/MyComponent.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+function MyComponent() {
+  const [car, setCar] = useState({
+    year: 2000,
+    make: 'Porsche',
+    model: '911 Turbo',
+  });
+
+  function handleYearChange(event) {
+    setCar((c) => ({ ...c, year: event.target.value }));
+  }
+
+  function handleMakeChange(event) {
+    setCar((c) => ({ ...c, make: event.target.value }));
+  }
+
+  function handleModelChange(event) {
+    setCar((c) => ({ ...c, model: event.target.value }));
+  }
+
+  return (
+    <div>
+      <p>
+        Your favourite car is: {car.year} {car.make} {car.model}
+      </p>
+      <input type="number" value={car.year} onChange={handleYearChange} />
+      <br />
+      <input type="tex" value={car.make} onChange={handleMakeChange} />
+      <br />
+      <input type="text" value={car.model} onChange={handleModelChange} />
+      <br />
+    </div>
+  );
+}
+
+export default MyComponent;
