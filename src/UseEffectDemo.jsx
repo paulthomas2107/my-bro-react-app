5c1494a1787135024cd3ef5d7342dd5925f936e8
Paul Thomas <paulthomas2107@gmail.com>
2024-01-07 14:13:59 +0000

    ddd

--- src/UseEffectDemo.jsx
+++ src/UseEffectDemo.jsx
@@ -3,10 +3,30 @@ import { useState, useEffect } from 'react';
 function UseEffectDemo() {
   const [count, setCount] = useState(0);
   const [color, setColor] = useState('green');
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
 
   useEffect(() => {
-    document.title = `Count is ${count}`;
-  }, [count]);
+    document.title = `Count is ${count} ${color} Size: ${width} ny ${height}`;
+    return () => {
+      // cleanup
+    };
+  }, [count, color, width, height]);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    console.log('ADD EL');
+    return () => {
+      // cleanup
+      window.removeEventListener('resize', handleResize);
+      console.log('REM EL');
+    };
+  }, []);
 
   function addCount() {
     setCount((c) => c + 1);
@@ -27,6 +47,8 @@ function UseEffectDemo() {
       <button onClick={subtractCount}>Subtract</button>
       <br />
       <button onClick={changeColor}>Change Color</button>
+      <p>Window Width: {width}px</p>
+      <p>Window Height: {height}px</p>
     </>
   );
 }
